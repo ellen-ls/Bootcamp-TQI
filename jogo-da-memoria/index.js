3ea656577a0ed2129a8bbb1ccb8de9f615b3c3a4
ellen-ls <ellen.l.s1996@gmail.com>
2022-06-26 16:48:02 -0300

    projeto jogo da memoria TQI

---
+++ jogo-da-memoria/index.js
@@ -0,0 +1,63 @@
+const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
+function flipCard(){
+    if(lockBoard) return;
+    if(this === firstCard) return;
+
+    this.classList.add('flip');
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
+}
+function checkForMatch(){
+    if(firstCard.dataset.card === secondCard.dataset.card){
+        disableCards();
+        return;
+    }
+    unflipCards();
+}
+
+function disableCards(){
+    firstCard.removeEventListenner('click', flipCard);
+    secondCard.removeEventListenner('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards(){
+    lockBoard = true;
+
+    setTimeout(()=>{
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    },1500)
+}
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard , secondCard] = [null, null];
+}
+
+(function shuffle(){
+    cards.forEach((card)=>{
+        let randomPosition = Math.floor(Math.random() * 12);
+        card.style.order = randomPosition;
+    })
+})()
+
+
+
+cards.forEach((card)=>{
+    card.addEventListener('click', flipCard)
+})
+
